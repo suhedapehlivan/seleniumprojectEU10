630dd26aa57118b5cfd5915ace655a204085ad77
suhedapehlivan <suheda.90@gmail.com>
2022-12-26 14:33:13 +0300

    tasks day3

---
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -0,0 +1,33 @@
+package com.cydeo.utilities;
+
+//TASK: NEW METHOD CREATION
+//Method name: getDriver
+//Static Method
+//Accepts String arg: browserType
+// - This arg will determine what type pf browser is opened
+// - if "chrome" passed --> it will open chrome browser
+// - if "firefox" passed --> it will open firefox browser
+//RETURN TYPE :"WebDriver"
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+        if (browserType.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else {
+            System.out.println("Given browser type does not exist/or is not currently supported");
+            System.out.println("Driver = null");
+            return null;
+        }
+    }
+
+}
