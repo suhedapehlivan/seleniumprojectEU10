2badc43dc171554e01c3b5e6800f8c2a26f423b1
suhedapehlivan <suheda.90@gmail.com>
2022-12-27 14:18:32 +0300

    tasks day3

---
+++ src/test/java/com/cydeo/tests/day3cssSelector_xpath/T5.java
@@ -0,0 +1,14 @@
+package com.cydeo.tests.day3cssSelector_xpath;
+
+public class T5 {
+    public static void main(String[] args) {
+        //TC #5: NextBaseCRM, locators, getText(), getAttribute() practice
+        //1- Open a chrome browser
+        //2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes 3- Enter incorrect username into login box:
+        //4- Click to `Reset password` button
+        //5- Verify “error” label is as expected
+        //Expected: Login or E-mail not found
+        //PS: Inspect and decide which locator you should be using to locate web elements.
+        //PS2: Pay attention to where to get the text of this button from
+    }
+}
