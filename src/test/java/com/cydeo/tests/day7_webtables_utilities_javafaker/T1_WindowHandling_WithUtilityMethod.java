715fe2d217573771f57ba076ad00b5fbafc6be33
suhedapehlivan <suheda.90@gmail.com>
2023-01-10 14:43:42 +0300

    day7

---
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling_WithUtilityMethod.java
@@ -0,0 +1,48 @@
+package com.cydeo.tests.day7_webtables_utilities_javafaker;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandling_WithUtilityMethod {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //TC #1: Window Handle practice
+        //1. Create new test and make setups
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void window_handling_test(){
+
+
+        //2. Goto:https://www.amazon.com
+        driver.get("https://www.amazon.com");
+
+        //3. Copy paste the lines from below into your class   OPENING NEW TABS
+        ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
+        //4. Create a logic to switch to the tab where Etsy.com is open
+
+        // 5. Assert:Title contains “Etsy”
+
+
+        BrowserUtils.switchWindowAndVerify(driver, "etsy","Etsy");
+
+
+        //Lines to be pasted:
+        //These lines will simply open new tabs using something called JavascriptExecutor and get those pages. We will learn JavascriptExecutor later as well.
+    }
+}
--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,4 +1,87 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.CRM_Utilities;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class T3_CRM_LOGIN {
+
+    public WebDriver driver;
+    //TC #3: Login scenario
+
+    //USERNAME helpdesk1@cybertekschool.com Helpdesk2@cybertekschool.com
+    //PASSWORD UserUser UserUser
+
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @Test
+    public void crm_login_test(){
+
+        //2. Goto:http://login1.nextbasecrm.com/
+        driver.get("http://login1.nextbasecrm.com/");
+
+        //3. Enter valid username
+
+        WebElement inputUsername= driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
+
+        //4. Enter valid password
+
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        //5. Click to`LogIn`button
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+        //6. Verify title is as expected:
+        // Expected: Portal
+        BrowserUtils.verifyTitle(driver,"My tasks");
+    }
+
+    @Test
+    public void crm_login_test_2(){
+
+        //2. Goto:http://login1.nextbasecrm.com/
+        driver.get("http://login1.nextbasecrm.com/");
+
+
+        //Calling my utility method to login helpdesk1
+        CRM_Utilities.crm_login(driver);
+
+        //6. Verify title is as expected:
+        // Expected: Portal
+        BrowserUtils.verifyTitle(driver,"My tasks");
+    }
+
+    @Test
+    public void crm_login_test_3(){
+
+        //2. Goto:http://login1.nextbasecrm.com/
+        driver.get("http://login1.nextbasecrm.com/");
+
+
+        //Calling my utility method to login helpdesk1
+        CRM_Utilities.crm_login(driver,"helpdesk2@cybertekschool.com", "UserUser" );
+
+        //6. Verify title is as expected:
+        // Expected: Portal
+
+        // BrowserUtils.sleep(2);
+        BrowserUtils.verifyTitle(driver,"(1) Portal");
+    }
+
 }
